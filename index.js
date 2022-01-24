f2127b7cfd40cf6f38b62c4763f4cd1580a9ca55
Rana Ameer <rana.h.amir.1998@gmail.com>
2022-01-24 13:19:41 +0200

    bug fixed

--- index.js
+++ index.js
@@ -1,52 +1,51 @@
 'use strict';
 
-const tasksOpen = {
-  rana: {
-    task: 'do the laundry',
-    details: 'wake up and do them in the morning',
-  },
-  user: {
-    task: '',
-    details: '',
-  },
-};
+// const tasksOpen = {
+//   rana: {
+//     task: 'do the laundry',
+//     details: 'wake up and do them in the morning',
+//   },
+//   user: {
+//     task: '',
+//     details: '',
+//   },
+// };
 
-const tasksTodo = {
-  rana: {
-    task: 'Course',
-    details: 'wake up and do them in the morning',
-  },
-  user: {
-    task: '',
-    details: '',
-  },
-};
+// const tasksTodo = {
+//   rana: {
+//     task: 'Course',
+//     details: 'wake up and do them in the morning',
+//   },
+//   user: {
+//     task: '',
+//     details: '',
+//   },
+// };
 
-const tasksPending = {
-  rana: {
-    task: 'Read',
-    details: 'wake up and do them in the morning',
-  },
-  user: {
-    task: '',
-    details: '',
-  },
-};
+// const tasksPending = {
+//   rana: {
+//     task: 'Read',
+//     details: 'wake up and do them in the morning',
+//   },
+//   user: {
+//     task: '',
+//     details: '',
+//   },
+// };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const user1 = {
-  id: 'todo',
   title: 'delectus aut autem',
   details: 'lorem epsum',
 };
+
 const user2 = {
-  id: 'pending',
   title: 'quis ut nam facilis et officia qui',
   details: 'loremm opsummmmmm',
 };
+
 const user3 = {
-  id: 'open',
   title: 'fugiat veniam minus',
   details: 'lorem opsummm',
 };
@@ -54,9 +53,8 @@ const user3 = {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const users = [user1, user2, user3];
-// tasksPending.push({ task: 'new task' });
-// console.log(tasksPending);
-const tasks = [tasksOpen, tasksTodo, tasksPending];
+
+// const tasks = [tasksOpen, tasksTodo, tasksPending];
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -65,6 +63,7 @@ const overlay = document.querySelector('.overlay');
 const add = document.querySelectorAll('.plus');
 const close = document.querySelector('.close-modal');
 const openTask = document.querySelector('.open-task');
+const todoTask = document.querySelector('.todo-task');
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -74,65 +73,72 @@ const btnTask = document.querySelector('.btn-add-task');
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-btnTask.addEventListener('click', function (e) {
-  e.preventDefault();
-  if (input.value !== '' && details.value !== '') {
-    const newInput = input.value;
-    const newDetails = details.value;
-    tasksOpen.user.task = newInput;
-    tasksOpen.user.details = newDetails;
-    console.log(tasksOpen);
-  } else return;
-});
+// btnTask.addEventListener('click', function (e) {
+//   e.preventDefault();
+//   if (input.value !== '' && details.value !== '') {
+//     const newInput = input.value;
+//     const newDetails = details.value;
+//     tasksOpen.user.task = newInput;
+//     tasksOpen.user.details = newDetails;
+//     console.log(tasksOpen);
+//   } else return;
+// });
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
+const displayTasks = function (task) {
+  todoTask.innerHTML = '';
+  task.forEach(function (mov) {
+    const html = `<div class="task">
+    <div class="task-list-title">
+      <h4>${mov.title}</h4>
+      <span class="delete-task">✖</span>
+    </div>
+    <p>${mov.details}</p>
+  </div>`;
 
+    todoTask.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayTasks(users);
+
+///////////////////////////////////////////////////////////////
 add.forEach(plus => {
   plus.addEventListener('click', function (e) {
     e.preventDefault();
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
-    if (input.value === '') return;
   });
 });
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-close.addEventListener('click', function (e) {
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+const closeModal = function (e) {
   e.preventDefault();
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
+};
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+btnTask.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (input.value === '' && details.value === '') return;
+  const object = {
+    title: input.value,
+    details: details.value,
+  };
+  users.push(object);
+  displayTasks(users);
+  closeModal(e);
+
+  input.value = details.value = '';
 });
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-const displayTasks = function (task) {
-  openTask.innerHTML = '';
-  task.forEach(function (mov) {
-    const html = `<div class="task">
-    <div class="task-list-title">
-      <h4>${mov.rana.task}</h4>
-      <span class="delete-task">✖</span>
-    </div>
-    <p>${mov.rana.details}</p>
-  </div>`;
-
-    openTask.insertAdjacentHTML('afterbegin', html);
-  });
-};
-
-displayTasks(tasks);
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+close.addEventListener('click', closeModal);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 const deleteTask = document.querySelectorAll('.delete-task');
+
 deleteTask.forEach(del => {
   del.addEventListener('click', function (e) {
-    e.preventDefault();
     e.target.closest('.task').remove();
   });
 });
-
-console.log(tasksOpen);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
